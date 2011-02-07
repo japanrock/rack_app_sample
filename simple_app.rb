31d20e8f512b3a62689c122d21a487174a1dc9d4
福重 伸太朗 <japanrock@fukushige-shintarou-no-iMac-2.local>
2011-02-07 14:19:38 +0900

    add simple_app

---
+++ simple_app.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+# SampleCode from http://gihyo.jp/dev/serial/01/ruby/0023
+
+class SimpleApp
+  def call(env)
+    p env
+    case env['REQUEST_METHOD']
+    when 'GET'
+      [
+        200,
+        { 'Content-Type' => 'text/html' },
+        ['<html><body><form method="POST"><input type="submit" value="見たい？" /></form></html>']
+      ]
+    when 'POST'
+      [
+        200,
+        { 'Content-Type' => 'text/html'},
+        ['<html><body>何見てんだよ</body></html>']
+      ]
+  end
+end
